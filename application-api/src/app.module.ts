a2e4b1e6de7d862317eb5088319ad8511ed12681
Igor Cardoso <iygorsilva@gmail.com>
2022-02-28 02:21:52 -0300

    TypeORM settings and install

--- application-api/src/app.module.ts
+++ application-api/src/app.module.ts
@@ -1,18 +1,18 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
-    })
-
+    }),
+    TypeOrmModule.forRoot()
   ],
   controllers: [],
   providers: [AppService],
--- docs/notes/notes.md
+++ docs/notes/notes.md
@@ -15,4 +15,16 @@
 
 ### GraphQL
 **Documentação**: https://docs.nestjs.com/graphql/quick-start
-yarn add @nestjs/graphql @nestjs/apollo graphql apollo-server-express
+*yarn add @nestjs/graphql @nestjs/apollo graphql apollo-server-express*
+
+
+### TypeORM
+**Documentação site Nestjs**: https://docs.nestjs.com/techniques/database#typeorm-integration
+**Instalação com PostgreSQL**
+*yarn add @nestjs/typeorm typeorm pg*
+
+**Documentação TypeORM**: https://typeorm.io/#/
+
+**Instalação para haver a comunincação com dotenv**
+*npm i --save @nestjs/config*
+
