5cf2aa86dc60fd2792ab84ba0872b063aaaca943
Igor Cardoso <iygorsilva@gmail.com>
2022-02-28 11:08:37 -0300

    dto settings and student nestjs-query

--- application-api/src/modules/students/dto/update-student.input.ts
+++ application-api/src/modules/students/dto/update-student.input.ts
@@ -3,6 +3,6 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateStudentInput extends PartialType(CreateStudentInput) {
-  @Field(() => Int)
-  id: number;
+  @Field(() => String)
+  id: string;
 }
--- application-api/src/modules/students/students.module.ts
+++ application-api/src/modules/students/students.module.ts
@@ -1,8 +1,25 @@
+import { UpdateStudentInput } from './dto/update-student.input';
+import { CreateStudentInput } from './dto/create-student.input';
+import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { Module } from '@nestjs/common';
-import { StudentsService } from './students.service';
-import { StudentsResolver } from './students.resolver';
+import { StudentDTO } from './dto/student.dto';
+import { Student } from './entities/student.entity';
 
 @Module({
-  providers: [StudentsResolver, StudentsService]
+  imports: [
+    // Configurações para nestjs-query (de onde buscar os dados)
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([Student])],
+      resolvers: [{
+        DTOClass: StudentDTO,
+        EntityClass: Student,
+        CreateDTOClass: CreateStudentInput,
+        UpdateDTOClass: UpdateStudentInput,
+        enableSubscriptions: true
+      }],
+    }),
+  ],
+  providers: []
 })
 export class StudentsModule {}
