4c937d4f73b79a739f3bf01aff717a190ca651a9
Igor Cardoso <iygorsilva@gmail.com>
2022-03-02 18:29:59 -0300

    bases module creation

---
+++ application-api/src/modules/bases/dto/base.dto.ts
@@ -0,0 +1,17 @@
+import { FilterableField } from '@nestjs-query/query-graphql';
+import { ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class BaseDTO {
+  @FilterableField()
+  id: string;
+
+  @FilterableField()
+  createAt: Date;
+
+  @FilterableField()
+  updateAt: Date;
+
+  @FilterableField()
+  deleteAt: Date;
+}
